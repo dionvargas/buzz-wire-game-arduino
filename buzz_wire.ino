e5250157e154d16c110a7d21ceb7ba16820863a0
dionvargas <dionathan_vargas@hotmail.com>
2022-08-30 14:27:49 -0300

    Version 1

---
+++ buzz_wire.ino
@@ -0,0 +1,183 @@
+
+int anel = 3;
+int buzzer = 6;
+int segE = 7;
+int segD = 8;
+int segC = 9;
+int segB = 13;
+int segA = 12;
+int segF = 11;
+int segG = 10;
+
+int vidas = 9;
+
+void setup()
+{
+  pinMode(anel, INPUT_PULLUP);
+  pinMode(buzzer, OUTPUT);
+  pinMode(segE, OUTPUT);
+  pinMode(segD, OUTPUT);
+  pinMode(segC, OUTPUT);
+  pinMode(segB, OUTPUT);
+  pinMode(segA, OUTPUT);
+  pinMode(segF, OUTPUT);
+  pinMode(segG, OUTPUT);
+
+  //Apresenta inicia o jogo
+  //mostrando 9 vidas no display
+  acende(vidas);
+}
+
+void loop()
+{
+  if (vidas >= 1) {
+
+    if (digitalRead(anel) == LOW) {
+      beep();
+      vidas--;
+      acende(vidas);
+    }
+  }
+  else {
+    gameOver();
+  }
+}
+
+void gameOver() {
+  while (true) {
+    apaga();
+    digitalWrite(segA, HIGH);
+    beep();
+    apaga();
+    digitalWrite(segB, HIGH);
+    beep();
+    apaga();
+    digitalWrite(segC, HIGH);
+    beep();
+    apaga();
+    digitalWrite(segD, HIGH);
+    beep();
+    apaga();
+    digitalWrite(segE, HIGH);
+    beep();
+    apaga();
+    digitalWrite(segF, HIGH);
+    beep();
+  }
+}
+
+void beep()
+{
+  tone(buzzer, 600);
+  delay(50);
+  noTone(buzzer);
+  delay(50);
+}
+
+void apaga() {
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segC, LOW);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+}
+
+void acende(int number) {
+  switch (number) {
+    case 0:
+      digitalWrite(segA, HIGH);
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, HIGH);
+      digitalWrite(segE, HIGH);
+      digitalWrite(segF, HIGH);
+      digitalWrite(segG, LOW);
+      break;
+    case 1:
+      digitalWrite(segA, LOW);
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, LOW);
+      digitalWrite(segE, LOW);
+      digitalWrite(segF, LOW);
+      digitalWrite(segG, LOW);
+      break;
+    case 2:
+      digitalWrite(segA, HIGH);
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, LOW);
+      digitalWrite(segD, HIGH);
+      digitalWrite(segE, HIGH);
+      digitalWrite(segF, LOW);
+      digitalWrite(segG, HIGH);
+      break;
+    case 3:
+      digitalWrite(segA, HIGH);
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, HIGH);
+      digitalWrite(segE, LOW);
+      digitalWrite(segF, LOW);
+      digitalWrite(segG, HIGH);
+      break;
+    case 4:
+      digitalWrite(segA, LOW);
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, LOW);
+      digitalWrite(segE, LOW);
+      digitalWrite(segF, HIGH);
+      digitalWrite(segG, HIGH);
+      break;
+    case 5:
+      digitalWrite(segA, HIGH);
+      digitalWrite(segB, LOW);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, HIGH);
+      digitalWrite(segE, LOW);
+      digitalWrite(segF, HIGH);
+      digitalWrite(segG, HIGH);
+      break;
+    case 6:
+      digitalWrite(segA, HIGH);
+      digitalWrite(segB, LOW);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, HIGH);
+      digitalWrite(segE, HIGH);
+      digitalWrite(segF, HIGH);
+      digitalWrite(segG, HIGH);
+      break;
+    case 7:
+      digitalWrite(segA, HIGH);
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, LOW);
+      digitalWrite(segE, LOW);
+      digitalWrite(segF, LOW);
+      digitalWrite(segG, LOW);
+      break;
+    case 8:
+      digitalWrite(segA, HIGH);
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, HIGH);
+      digitalWrite(segE, HIGH);
+      digitalWrite(segF, HIGH);
+      digitalWrite(segG, HIGH);
+      break;
+    case 9:
+      digitalWrite(segA, HIGH);
+      digitalWrite(segB, HIGH);
+      digitalWrite(segC, HIGH);
+      digitalWrite(segD, HIGH);
+      digitalWrite(segE, LOW);
+      digitalWrite(segF, HIGH);
+      digitalWrite(segG, HIGH);
+      break;
+    default:
+      apaga();
+      break;
+  }
+}
